6966311fbb0b561d7e85dd6c42979d8306813275
carolinesoaress <eicarolsoares@gmail.com>
2023-10-24 21:04:18 -0300

    alteração strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,10 @@ int ft_strncmp(const char *str1, const char *str2, size_t n)
     int i = 0;  
 
     while(str1[i] == str2[i] && n > 0)
-    {
+    {   
+        if (str1[i] == '\0' || n == 1) {
+            return 0; // Strings são iguais até o final ou até o limite de caracteres
+        }
         i++;
         n--;
     }
