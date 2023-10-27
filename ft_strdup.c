ea2027079500af5c73eea62d359e3e8c5a1d69ca
carolinesoaress <eicarolsoares@gmail.com>
2023-10-27 18:55:44 -0300

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+
+char *ft_strdup(const char *str)
+{
+    char *cpy;
+    size_t i = 0;
+
+    size_t len = ft_strlen(str) + 1;
+
+    cpy = (char *)malloc(len);
+
+    if(!cpy)
+        return (NULL);
+
+    while (str[i] != '\0')
+    {
+        cpy[i] = str[i];
+        i++;
+    }
+    cpy[i] = '\0';
+    return (cpy);
+}
+int main() {
+    const char *original = "Hello, world!";
+    char *copy = ft_strdup(original);
+    
+    if (copy != NULL) {
+        printf("Original: %s\n", original);
+        printf("Cópia: %s\n", copy);
+        
+        // Lembre-se de liberar a memória alocada dinamicamente quando não for mais necessária
+        free(copy);
+    } else {
+        printf("Falha na alocação de memória.\n");
+    }
+    
+    return 0;
+}
--- libft.h
+++ libft.h
@@ -20,5 +20,6 @@ void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *ptr1, const void *ptr2, size_t n);
 char *ft_strnstr(const char *src, const char *sub, size_t n);
 void *ft_calloc(size_t num_elements, size_t element_size);
+char *ft_strdup(const char *str);
 
 #endif
