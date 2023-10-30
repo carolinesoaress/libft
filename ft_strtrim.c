77b1c42dc0f7de642f5514fc06f48699fd99b13c
carolinesoaress <eicarolsoares@gmail.com>
2023-10-30 00:23:05 -0300

    ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+    if (s1 == NULL || set == NULL) {
+        return NULL;
+    }
+
+    size_t start = 0;
+    size_t end = ft_strlen(s1);
+
+    while (s1[start] != '\0' && ft_strchr(set, s1[start]) != NULL) {
+        start++;
+    }
+
+    if (start == end) {
+        // A string consiste apenas de caracteres a serem removidos.
+        // Nesse caso, retorne uma string vazia.
+        return strdup("");
+    }
+
+    while (end > start && strchr(set, s1[end - 1]) != NULL) {
+        end--;
+    }
+
+    size_t trimmed_length = end - start;
+    char *trimmed_str = (char *)malloc(trimmed_length + 1);
+
+    if (trimmed_str == NULL) {
+        return NULL; // Falha na alocação de memória.
+    }
+
+    strncpy(trimmed_str, s1 + start, trimmed_length);
+    trimmed_str[trimmed_length] = '\0';
+
+    return trimmed_str;
+}
--- libft.h
+++ libft.h
@@ -6,21 +6,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void *ft_memset(void *dest, int c, size_t n);
-void *ft_memcpy(void *dest, const void *org, size_t n);
-void ft_bzero(void *s, size_t len);
-size_t ft_strlen(const char *str);
-size_t ft_strlcpy(char *dest, const char *src, size_t size);
-size_t ft_strlcat(char *dst, const char *src, size_t size);
-int ft_toupper(int c);
-int ft_tolower(int c);
-char *ft_strchr(const char *str, int c);
-int ft_strncmp(const char *str1, const char *str2, size_t n);
-void *ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *ptr1, const void *ptr2, size_t n);
-char *ft_strnstr(const char *src, const char *sub, size_t n);
-void *ft_calloc(size_t num_elements, size_t element_size);
-char *ft_strdup(const char *str);
-char *ft_substr(char const *s, unsigned int start, size_t len);
-
+void    *ft_memset(void *dest, int c, size_t n);
+void    *ft_memcpy(void *dest, const void *org, size_t n);
+void    ft_bzero(void *s, size_t len);
+size_t  ft_strlen(const char *str);
+size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
+char    *ft_strchr(const char *str, int c);
+int     ft_strncmp(const char *str1, const char *str2, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *ptr1, const void *ptr2, size_t n);
+char    *ft_strnstr(const char *src, const char *sub, size_t n);
+void    *ft_calloc(size_t num_elements, size_t element_size);
+char    *ft_strdup(const char *str);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
 #endif
