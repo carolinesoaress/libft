bcebfb58a4604187a8327f960d44a650399447f1
Caroline Soares <104658660+carolinesoaress@users.noreply.github.com>
2023-10-30 15:29:29 -0300

    Update ft_strncmp.c
    TESTE QUE ESTA FALAHANDO

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -15,33 +15,13 @@ int ft_strncmp(const char *str1, const char *str2, size_t n)
     }
     return (str1[i] - str2[i]);
 }
-/*int main() {
-    char str1[] = "banani verde";
-    char str2[] = "banano";
+int main() {
+    char *str1 = "test\200";
+    char *str2 = "test\0";
 
-    int result = ft_strncmp(str1, str2,7);
-
-    if (result == 0) {
-        printf("ft_strncmp: As strings são iguais .\n");
-    } else if (result < 0) {
-        printf("ft_strncmp: str1 é menor que str2 \n");
-    } else {
-        printf("ft_strncmp: str1 é maior que str2 \n");
-    }
-
-    int result2 = strncmp(str1, str2,7);
-
-    if (result == 0) {
-        printf("strncmp: As strings são iguais \n");
-    } else if (result < 0) {
-        printf("strncmp: str1 é menor que str2 \n");
-    } else {
-        printf("strncmp: str1 é maior que str2 \n");
-    }
-
-
-
-
-
-    return 0;
-}*/
+    int result = ft_strncmp(str1, str2,6);
+	printf("Minha: %d\n", result);
+	int res = strncmp(str1, str2,6);
+	printf("Original: %d\n", res);
+	return 0;
+}
